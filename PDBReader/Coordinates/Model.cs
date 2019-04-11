80bbe017788ea2c4599d23f3be76605134ac1b58
Asher Simpson <asher.simpson@outlook.com>
2019-04-11 11:23:08 -0400

    Added basic model class. Added a text file to ease my brain.

---
+++ PDBReader/Coordinates/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDBReader.Coordinates
+{
+    class Model
+    {
+
+        private int _serial;
+
+        public Model(int serial)
+        {
+            _serial = serial;
+        }
+
+        public int GetSerial() { return _serial; }
+
+    }
+}
