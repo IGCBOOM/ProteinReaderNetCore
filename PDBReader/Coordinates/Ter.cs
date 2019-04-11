b85fec0cf2c5f73b8d46d48b5e035e0836c7d0d3
Asher Simpson <asher.simpson@outlook.com>
2019-04-11 18:41:50 -0400

    Tadaa! File name fixed

---
+++ PDBReader/Coordinates/Ter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDBReader.Coordinates
+{
+    class Ter
+    {
+
+        private string _atomserial;
+        private string _resname;
+        private char _chainid;
+        private uint _resseq;
+        private char _rescode;
+
+        public Ter(string atomserial, string resname, char chainid, uint resseq, char rescode)
+        {
+            _atomserial = atomserial;
+            _resname = resname;
+            _chainid = chainid;
+            _resseq = resseq;
+            _rescode = rescode;
+        }
+
+        public string GetAtomSerial() { return _atomserial; }
+        public string GetResName() { return _resname; }
+        public char GetChainID() { return _chainid; }
+        public uint GetResSeq() { return _resseq; }
+        public char GetResCode() { return _rescode; }
+
+    }
+}
