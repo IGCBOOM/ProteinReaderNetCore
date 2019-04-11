5b0bfc41748d35b9583ec111deeac27e63b2ed90
IGCBOOM <caya1.carter1@gmail.com>
2019-04-10 20:24:58 -0500

    Add Coor Type Anisou.

--- PDBReader/Coordinates/Anisou.cs
+++ PDBReader/Coordinates/Anisou.cs
@@ -12,6 +12,34 @@ namespace PDBReader.Coordinates
         private char _altloc;
         private string _resname;
         private char _chainid;
+        private uint _resseq;
+        private char _icode;
+        private int _i1;
+        private int _i2;
+        private int _i3;
+        private int _i4;
+        private int _i5;
+        private int _i6;
+        private string _vanity;
+        private int _charge;
 
+        public Anisou(uint serial, string atomname, char altloc, string resname, char chainid, uint resseq, char icode, int i1, int i2, int i3, int i4, int i5, int i6, string vanity, int charge)
+        {
+            _serial = serial;
+            _atomname = atomname;
+            _altloc = altloc;
+            _resname = resname;
+            _chainid = chainid;
+            _resseq = resseq;
+            _icode = icode;
+            _i1 = i1;
+            _i2 = i2;
+            _i3 = i3;
+            _i4 = i4;
+            _i5 = i5;
+            _i6 = i6;
+            _vanity = vanity;
+            _charge = charge;
+        }
     }
 }
