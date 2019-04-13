bea48da827232d3664809d7ebe0443a282b1f5c6
IGCBOOM <caya1.carter1@gmail.com>
2019-04-13 11:03:14 -0500

    Add mmCIF Reader csproj.

---
+++ mmCIFReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mmCIFReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
